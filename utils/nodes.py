a46f137c2835a2293efa6e1c05ac291919458dc6
Jellyfish Hosting <143347621+JellyfishHosting@users.noreply.github.com>
2023-09-07 22:11:47 +0100

    Rewrite time
    As said in our annoucement, the dashboard will be going through a complete rewrite.

--- utils/nodes.py
+++
@@ -1,6 +0,0 @@
-from pydactyl import PterodactylClient
-api = PterodactylClient('http://149.102.148.225/', 'ptla_oOM44PX5bryFwSEl9rUS8Xm91S0Dzeq5IAFs88Ll0RA')
-
-def get_node_details(nodeid):
-    result = api.nodes.get_node_details(nodeid)
-    return result
--- utils/server.py
+++
@@ -1,5 +0,0 @@
-from pydactyl import PterodactylClient
-api = PterodactylClient('http://149.102.148.225/', 'ptla_oOM44PX5bryFwSEl9rUS8Xm91S0Dzeq5IAFs88Ll0RA')
-
-def create_server(name, user_id, nest_id, egg_id, memory_limit, storage_limit, location_ids, cpu_limit, description):
-    api.servers.create_server(name=name, user_id=user_id, nest_id=nest_id, egg_id=egg_id, memory_limit=memory_limit, cpu_limit=cpu_limit, description=description, swap_limit=0, backup_limit=20, database_limit=2, disk_limit=storage_limit, location_ids=location_ids)
