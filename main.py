d98b1744893ad0e338c5ce2f51d54ae21fac2e39
Jellyfish Hosting <143347621+JellyfishHosting@users.noreply.github.com>
2023-09-07 23:10:14 +0100

    Update main.py
    Adding comments to code.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 import config
 import importlib
 app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECERT_KEY
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+app.config['SECRET_KEY'] = config.SECERT_KEY							# Sets the sessions secret_key. Randomly generated every time the app is restarted.
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)		# Sets the cookie / session lifetime to 30 minutes, meaning every 30 minutes its cleared.
 # Get a list of all files in the blueprints folder
 blueprint_folder = "blueprints"
 blueprint_files = [f for f in os.listdir(blueprint_folder) if f.endswith('.py') and not f.startswith("__")]
@@ -21,13 +21,15 @@ for blueprint_file in blueprint_files:
 		app.register_blueprint(blueprint_module.bp)
 		print("Loaded a blueprint")
 
+# Only executes when the 404 error is catched. (Page not Found)
 @app.errorhandler(404)
 def not_found(error):
-	return render_template('404.html')
+	return render_template('404.html')		# Renders the 404 template in the templates folder.
 
+# Only executes when the 500 error is catched. (Internal Server Error)
 @app.errorhandler(500)
 def internal_error(error):
-	return render_template('500.html')
+	return render_template('500.html')		# Renders the 500 template in the templates folder
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
