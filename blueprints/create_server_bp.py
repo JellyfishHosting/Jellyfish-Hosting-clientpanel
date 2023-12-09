9cca43f5576fcf49d6217b78b9275ae8c4e5c398
Jellyfish Hosting <143347621+JellyfishHosting@users.noreply.github.com>
2023-12-09 16:53:13 +0000

    Update create_server_bp.py
    Adding the queue system.

--- blueprints/create_server_bp.py
+++ blueprints/create_server_bp.py
@@ -4,12 +4,18 @@ import utils.server
 from utils.nodes import get_node_details
 from zenora import APIClient
 import flask_pymongo
-from config import mongo_uri
+from config import mongo_uri    
 import json
+from apscheduler.schedulers.background import BackgroundScheduler
 import utils.users
 import datetime
+import time
+import threading
 mongodb_client = flask_pymongo.pymongo.MongoClient(mongo_uri)
 mydb = mongodb_client['jellyfishhost']
+queue_collection = mydb['server_queue']
+serversCollection = mydb['servers']
+nodeCollections = mydb['nodes']
 
 LOCATION_IDS = {"uk": 1, "de": 2}
 NODE_IDS = {"UK1": 1, "DE1": 2}
@@ -32,6 +38,45 @@ Functionality:
 - Handles success and error cases.
 - Renders create server page or redirects on completion.
 """
+
+def process_server_queue():
+    if queue_collection.count_documents({}) > 0:
+        server_info = queue_collection.find_one({})
+        print(server_info)
+        server_name = server_info.get('name')
+        node_id = server_info.get('node_id')
+        print(node_id)
+        nodeid = str(node_id)
+        print(nodeid)
+        data = nodeCollections.find_one({'nodeid': nodeid})
+        print(data)
+        slots = data.get('slots')
+        available_slots = int(slots)
+        if available_slots <= 0:
+            return
+        else:
+            try:
+                result = utils.server.create_server(name=server_info['name'], user_id=server_info['user_id'], ram=server_info['ram'], nestid=server_info['nestid'], eggid=server_info['eggid'], storage=server_info['storage'], location_ids=server_info['location_ids'], cpu_limit=server_info['cpu_limit'], description=server_info['description'])
+                result_content = result.content
+                response_data = json.loads(result_content)
+                server_id = response_data['attributes']['id']
+                serversCollection.insert_one({
+                    "server_name": server_info['name'],
+                    'cpu': server_info['cpu_limit'],
+                    'ram': server_info['ram'],
+                    'storage': server_info['storage'],
+                    "email": server_info['email'],
+                    "server_id": server_id,
+                    "renewal_date": server_info['renewal_date']
+                })
+                queue_collection.delete_one({'name': server_name})
+            except Exception as e:
+                print(f"Error: There was an issue when creating the server. {e}")
+            time.sleep(1)
+
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(process_server_queue, 'interval', minutes=30)
+scheduler.start()
 
 bp = Blueprint('create_server', __name__, template_folder='templates')
 @bp.route('/create_server', methods=['GET', 'POST'])
@@ -106,22 +151,46 @@ def create_server():
             formatted_renewal_date = current_date + datetime.timedelta(days=7)
             date_format = '%Y-%m-%d'
             renewal_date = formatted_renewal_date.strftime(date_format)
-            serversCollection = mydb['servers']
             filter = {'email': email}
             newValues = { "$set": { 'cpu_limit': cpu_limit_db, 'memory_limit': ram_limit_db, 'server_limit': server_limit_db, 'storage_limit': storage_limit_db}}
             usersCollections.update_one(filter, newValues)
-            try:
-                result = utils.server.create_server(name=server_name, user_id=user_id, nestid=nestid, eggid=eggid, ram=ram, storage=storage, location_ids=location_ids, cpu_limit=cpu_limit, description=description)
-                resultContent = result.content
-                requests = json.loads(resultContent)
-                server_id = requests['attributes']['id']
-                serversCollection.insert_one({"server_name": server_name, "cpu": cpu_limit, "ram": ram, "storage": storage, "email": email, 'server_id': server_id, 'renewal_date': renewal_date})
-                flash('Success: Server created successfully!', 'success')
-                return redirect(url_for('dashboard.dashboard'))
-            except Exception as e:
-                print(e)
-                flash('Error: There was an issue when creating the server! Please contact the main developers.', 'error')
-                return render_template('create_server.html')
+            server_node_id = str(nodeid)
+            dataa = nodeCollections.find_one({'nodeid': server_node_id})
+            print(dataa)
+            available_slots = dataa.get('slots')
+            available_slotss = int(available_slots)
+            if available_slotss > 0:
+                try:
+                    result = utils.server.create_server(name=server_name, user_id=user_id, nestid=nestid, eggid=eggid, ram=ram, storage=storage, location_ids=location_ids, cpu_limit=cpu_limit, description=description)
+                    resultContent = result.content
+                    requests = json.loads(resultContent)
+                    server_id = requests['attributes']['id']
+                    serversCollection.insert_one({"server_name": server_name, "cpu": cpu_limit, "ram": ram, "storage": storage, "email": email, 'server_id': server_id, 'renewal_date': renewal_date})
+                    available_slotss -= 1
+                    available_slotss = str(available_slotss)
+                    nodeCollections.update_one({'nodeid': server_node_id}, {'slots': available_slotss})
+                    flash('Success: Server created successfully!', 'success')
+                    return redirect(url_for('dashboard.dashboard'))
+                except Exception as e:
+                    print(e)
+                    flash('Error: There was an issue when creating the server! Please contact the main developers.', 'error')
+                    return render_template('create_server.html')
+            else:
+                queue_collection.insert_one({
+                    'name': server_name,
+                    'user_id': user_id,
+                    'nestid': nestid,
+                    'eggid': eggid,
+                    'ram': ram,
+                    'storage': storage,
+                    'location_ids': location_ids,
+                    'node_id': nodeid,
+                    'cpu_limit': cpu_limit,
+                    'description': description,
+                    'email': email,
+                    'renewal_date': renewal_date
+                })
+                flash('Success: Server added to our queue. Your server will be created when the selected node has another slot available.')
     else:
         return redirect(url_for('login.login'))
     return render_template('create_server.html')
